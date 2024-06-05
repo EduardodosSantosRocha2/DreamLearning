dfa6965057e66e9307105b2b6fec313996b91599
Eduardo dos Santos <duduredtone@gmail.com>
2024-06-05 17:25:56 -0300

    frontend

--- TCC/DuLearning/static/script/regression.js
+++ TCC/DuLearning/static/script/regression.js
@@ -1,3 +1,25 @@
+
+function stars(){
+    let count = 500;
+    let scene = document.querySelector(".scene");
+    let i = 0;
+    while(i < count){
+        let star = document.createElement("i");
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let y = Math.floor(Math.random() * window.innerHeight);
+        let duration = Math.random() * 10;
+        let size = Math.random() * 2;
+        star.style.left = x + 'px';
+        star.style.top = y + 'px';
+        star.style.width = 1 + size + 'px';
+        star.style.height = 1 + size + 'px';
+        star.style.animationDuration = 5 + duration + 's';
+        star.style.animationDelay = duration + 's';
+        scene.appendChild(star)
+        i++;
+    }
+}
+stars();
 document.addEventListener("DOMContentLoaded", function () {
     var separator = "";
     var csvFileInput = document.getElementById("csv_file");
@@ -40,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 parametersInput.type = parametersColection[keys[i]];
                 parametersInput.step = "0.0000000001";
                 parametersInput.name = "parameters" + (i + 1);
+                parametersInput.classList.add("nes-input");
                 parametersDiv.appendChild(parametersInput);
                 parametersDiv.appendChild(document.createElement("br"));
             }
